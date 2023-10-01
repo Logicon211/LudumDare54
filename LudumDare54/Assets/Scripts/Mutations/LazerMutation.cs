086bc2c70871f9ad2c376827c74134fb6a4c8fb6
MitchellMaclean <heavyiam@hotmail.com>
2023-09-30 22:58:43 -0300

    Beginning of lazer mutation

---
+++ LudumDare54/Assets/Scripts/Mutations/LazerMutation.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LazerMutations : Mutation
+{
+
+public GameObject lazerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void useAbility() {
+        Debug.Log("TODO: setup punch ability");
+        //Instantiate lazer prefab
+    }
+}

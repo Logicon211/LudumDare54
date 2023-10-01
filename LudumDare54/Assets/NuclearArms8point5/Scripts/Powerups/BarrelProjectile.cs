1d11195929185b5b6945fd32b09431d19c02d6cd
Logan Marks <logicon211@gmail.com>
2023-10-01 11:46:56 -0300

    fixing extra barrel projecile

--- LudumDare54/Assets/NuclearArms8point5/Scripts/Powerups/BarrelProjectile.cs
+++ LudumDare54/Assets/NuclearArms8point5/Scripts/Powerups/BarrelProjectile.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class BarrelProjectile : MonoBehaviour {
+public class BarrelProjectileOLD : MonoBehaviour {
 
     public GameObject explosion;
     public GameObject teleport;

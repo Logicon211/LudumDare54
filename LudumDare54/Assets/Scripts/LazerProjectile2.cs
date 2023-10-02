02198e7a36b048ae10d928353c00d98e0dc2d0c3
Logan Marks <logicon211@gmail.com>
2023-10-01 21:50:22 -0300

    Laser doesn't use hitbox anymore

--- LudumDare54/Assets/Scripts/LazerProjectile2.cs
+++ LudumDare54/Assets/Scripts/LazerProjectile2.cs
@@ -11,6 +11,11 @@ public class LaserProjectile2 : MonoBehaviour
 
     private BoxCollider2D boxCollider;
 
+    public Tile originTile;
+    private Tile damageTile1;
+    private Tile damageTile2;
+    private Tile damageTile3;
+
 private ContactFilter2D filter;
 
     void Start()
@@ -26,18 +31,44 @@ private ContactFilter2D filter;
     // Update is called once per frame
     void Update()
     {
+
     }
 
     public void damageEnemies(){
-        List<Collider2D> results = new List<Collider2D>();
-        boxCollider.OverlapCollider(filter, results);
-        foreach(Collider2D collision in results) {
-            if (collision) {
-                if (collision.gameObject.tag == "Enemy") {
-                    EnemyAi enemy = collision.gameObject.GetComponent<EnemyAi>();
-                    enemy.Damage(attackDamage);
-                }
-            }
+        // List<Collider2D> results = new List<Collider2D>();
+        // boxCollider.OverlapCollider(filter, results);
+        // foreach(Collider2D collision in results) {
+        //     if (collision) {
+        //         if (collision.gameObject.tag == "Enemy") {
+        //             EnemyAi enemy = collision.gameObject.GetComponent<EnemyAi>();
+        //             enemy.Damage(attackDamage);
+        //         }
+        //     }
+        // }
+        if(damageTile1){
+            damageTile1.Damage(attackDamage);
+        }
+        if(damageTile2){
+            damageTile2.Damage(attackDamage);
+        }
+        if(damageTile3){
+            damageTile3.Damage(attackDamage);
+        }
+    }
+
+    public void SetDamagingTiles(Tile tilein) {
+        BattleGrid grid = tilein.GetGrid();
+        int x = tilein.gridX;
+        int y = tilein.gridY;
+
+        if(x + 1 < 6) {
+            damageTile1 = grid.grid[x+1, y];
+        }
+        if(x + 2 < 6) {
+            damageTile2 = grid.grid[x+2, y];
+        }
+        if(x + 3 < 6) {
+            damageTile3 = grid.grid[x+3, y];
         }
     }
 
